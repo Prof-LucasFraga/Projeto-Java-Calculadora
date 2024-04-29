6220c45a4a512b19e23e8881700c56ee7b024a3e
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-04-29 07:35:35 -0300

    Remoção dos comentários gerados pela IDE

--- ProjetoCalculadora/src/projetocalculadora/ProjetoCalculadora.java
+++ ProjetoCalculadora/src/projetocalculadora/ProjetoCalculadora.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projetocalculadora;
 
-/**
- *
- * @author tiluc
- */
 public class ProjetoCalculadora {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao projeto de Calculadora");
     }
